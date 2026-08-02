b62ab8337115df53ac845045b0c24eedd07dee62
generalgroovy <41192316+generalgroovy@users.noreply.github.com>
2026-08-02 11:33:47 +0000

    Fix installed controller module path

--- traktor-controller.py
+++ traktor-controller.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path.home() / ".local/lib/traktor-system-controller"))
+
 from traktor_controller.cli import main
 
 if __name__ == "__main__":
